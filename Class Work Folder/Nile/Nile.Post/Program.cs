47f5fe1deb663630d6b3aee24e4ea7150d8e80e3
Harrison.Suffka@my.tccd.edu <Harrison.Suffka@my.tccd.edu>
2017-09-06 19:16:06 -0500

    Day 2

--- Class Work Folder/Nile/Nile.Post/Program.cs
+++ Class Work Folder/Nile/Nile.Post/Program.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿/*
+ * Harrison Suffka
+ * ITSE 1430
+ * Nile.Host
+ */ 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Nile.Post {
-    class Program {
+namespace Nile.Post 
+    {
+    class Program 
+        {
         static void Main( string[] args )
         {
+
         }
     }
 }
