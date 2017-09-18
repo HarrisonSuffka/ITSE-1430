36f44aa703836fc403dc8265d94149d823e786ad
Harrison.Suffka@my.tccd.edu <Harrison.Suffka@my.tccd.edu>
2017-09-18 17:35:56 -0500

    8/18

--- Class Work Folder/Section 2/Nile/Nile.Post/Properties/AssemblyInfo.cs
+++ Class Work Folder/Section 2/Nile/Nile.Post/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyTitle("Nile.Post")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyProduct("Nile.Post")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7f97f5a3-efd1-4e93-a90f-617db00d2156")]
+[assembly: Guid("17b8de35-9413-46ba-adc0-25de754b852f")]
 
 // Version information for an assembly consists of the following four values:
 //
