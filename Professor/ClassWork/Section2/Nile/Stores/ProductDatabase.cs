3cf791af5cdd947debb0646b9c4eaa1605224add
Harrison.Suffka@my.tccd.edu <Harrison.Suffka@my.tccd.edu>
2017-10-23 17:08:34 -0500

    10/23/2017

--- Professor/ClassWork/Section2/Nile/Stores/ProductDatabase.cs
+++ Professor/ClassWork/Section2/Nile/Stores/ProductDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,36 +9,26 @@ namespace Nile.Stores
 {
     /// <summary>Base class for product database.</summary>
     public abstract class ProductDatabase : IProductDatabase
-    {
-
+    {        
         /// <summary>Adds a product.</summary>
         /// <param name="product">The product to add.</param>
         /// <returns>The added product.</returns>
-        public Product Add( Product product )
+        public Product Add ( Product product )
         {
-            //Validate
+            //TODO: Validate
             if (product == null)
                 return null;
 
             //Using IValidatableObject
             if (!ObjectValidator.TryValidate(product, out var errors))
-            return null;
-
+                return null;
             //if (!String.IsNullOrEmpty(product.Validate()))
             //    return null;
 
+            //Emulate database by storing copy
             return AddCore(product);
 
-            //Emulate database by storing copy 
-            //var newProduct = CopyProduct(product);
-                //_products.Add(product);
-                //newProduct.Id = _nextId++;
-            //AddCore(newProduct);
-
-            //return CopyProduct(newProduct)
-            
-            //var item = _products[0] as Product;
-            //var item = _products[0];
+            //var item = _list[0];
 
             //TODO: Implement Add
             //return product;
@@ -47,78 +38,94 @@ namespace Nile.Stores
         /// <returns>The product, if it exists.</returns>
         public Product Get ( int id )
         {
+            //TODO: Validate
             if (id <= 0)
                 return null;
 
-            //foreach ( var product in _products)
-            //{
-            //    if (product.Id == id)
-            //        return CopyProduct(product);
-            //}
-
-            //return (product != null) ? CopyProduct(product) : null;
             return GetCore(id);
         }
-
-        protected abstract Product GetCore( int id );
-
+        
         /// <summary>Gets all products.</summary>
         /// <returns>The products.</returns>
-        public IEnumerable<Product> GetAll()
+        public IEnumerable<Product> GetAll ()
         {
             return GetAllCore();
+
+            #region Ignore
+
+            //How many products?
+            //var count = 0;
+            //foreach (var product in _products)
+            //{
+            //    if (product != null)
+            //        ++count;
+            //};
+
+            //var items = new Product[count];
+            //var index = 0;
+
+            //foreach (var product in _products)
+            //{
+            //    if (product != null)
+            //        //product = new Product();
+            //        items[index++] = CopyProduct(product);
+            //};
+
+            //return items;
+            #endregion
         }
-
-        protected abstract IEnumerable<Product> GetAllCore();
-
+        
         /// <summary>Removes the product.</summary>
-        /// <param name="product">The product to remove.</param>
-        public void Remove( int id )
+        /// <param name="id">The product to remove.</param>
+        public void Remove ( int id )
         {
+            //TODO: Validate
             if (id <= 0)
                 return;
 
-            //var product = GetCore(id);
-            //if (product.Id == id)
-            //    return RemoveCore(product);
             RemoveCore(id);
 
+            //if (_list[index].Name == product.Name)
+            //{
+            //    _list.RemoveAt(index);
+            //    break;
+            //};        
         }
-
-        protected abstract void RemoveCore( int id );
-
+        
         /// <summary>Updates a product.</summary>
         /// <param name="product">The product to update.</param>
         /// <returns>The updated product.</returns>
-        public Product Update( Product product )
+        public Product Update ( Product product )
         {
-
-           if (product == null)
-                    return null;
+            //TODO: Validate
+            if (product == null)
+                return null;
 
             //Using IValidatableObject
             if (!ObjectValidator.TryValidate(product, out var errors))
                 return null;
-
             //if (!String.IsNullOrEmpty(product.Validate()))
-            //     return null;
+            //    return null;
 
-            //get existing product 
+            //Get existing product
             var existing = GetCore(product.Id);
             if (existing == null)
                 return null;
 
-            //_products.Remove(existing);
-            ////Emulate database by storing copy 
-            // var newProduct = CopyProduct(product);
-            //_products.Add(product);
-
-            return UpdateCore( existing ,product);
+            return UpdateCore(existing, product);
         }
+
+        #region Protected Members
+
+        protected abstract Product GetCore( int id );
+
+        protected abstract IEnumerable<Product> GetAllCore();
+
+        protected abstract void RemoveCore( int id );
 
         protected abstract Product UpdateCore( Product existing, Product newItem );
 
         protected abstract Product AddCore( Product product );
-
+        #endregion
     }
 }
