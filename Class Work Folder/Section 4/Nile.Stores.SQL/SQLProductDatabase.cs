09f8e31a5889f11f63337494b81631901c13f50e
Harrison.Suffka@my.tccd.edu <Harrison.Suffka@my.tccd.edu>
2017-11-15 18:31:10 -0600

    11/15/2015

--- Class Work Folder/Section 4/Nile.Stores.SQL/SQLProductDatabase.cs
+++ Class Work Folder/Section 4/Nile.Stores.SQL/SQLProductDatabase.cs
@@ -24,56 +24,122 @@ namespace Nile.Stores.SQL
 
         protected override Product AddCore( Product product )
         {
-            throw new NotImplementedException();
+            var id = 0;
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("AddProduct", conn) { CommandType = CommandType.StoredProcedure };
+
+                cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = product.Name;
+                cmd.Parameters.AddWithValue("@Description", product.Description);
+                cmd.Parameters.AddWithValue("@price", product.Price);
+                cmd.Parameters.AddWithValue("@isDiscontinued", product.IsDiscontinued);
+
+                id = Convert.ToInt32(cmd.ExecuteScalar());
+
+            };
+            return GetCore(id);
         }
 
         protected override IEnumerable<Product> GetAllCore()
         {
+            var products = new List<Product>();
             using (var connection = OpenDatabase())
             {
+                //connection.CreateCommand();
                 var cmd = new SqlCommand("GetAllProducts", connection);
                 cmd.CommandType = CommandType.StoredProcedure;
 
-                var products = new List<Product>();
-
                 using (var reader = cmd.ExecuteReader())
                 {
-                    while( reader.Read())
+                    while (reader.Read())
                     {
                         //reader.GetName(0);
                         //reader.GetFieldType(1);
                         //Convert.ToInt32(reader["Id"]);
-
-                        var product = new Product() 
-                        {
-                            Id = reader.IsDBNull(0) ? 0 :reader.GetInt32(0),
+                        var product = new Product() {
+                            Id = reader.IsDBNull(0) ? 0 : reader.GetInt32(0),
                             Name = reader.GetFieldValue<string>(1),
-                            Description = reader.GetString(3),
                             Price = reader.GetDecimal(2),
+                            Description = reader.GetString(3),
                             IsDiscontinued = reader.GetBoolean(4)
                         };
                         products.Add(product);
                     };
                 };
 
-                    return new Product[0];
+                return products;
             };
 
         }
 
         protected override Product GetCore( int id )
         {
-            throw new NotImplementedException();
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("GetProduct", conn) { CommandType = CommandType.StoredProcedure };
+
+                cmd.Parameters.AddWithValue("@id", id);
+
+                var ds = new DataSet();
+                var da = new SqlDataAdapter()
+                {
+                    SelectCommand = cmd
+                };
+
+                da.Fill(ds);
+
+                var table = ds.Tables.OfType<DataTable>().FirstOrDefault();
+                if (table != null)
+                {
+                    var row = table.AsEnumerable().FirstOrDefault();
+                    if (row != null)
+                    {
+                        return new Product() {
+                            Id = Convert.ToInt32(row["Id"]),
+                            Name = row.Field<string>("Name"),
+                            Description = row.Field<string>("Description"),
+                            Price = row.Field<decimal>("Price"),
+                            IsDiscontinued = row.Field<bool>("isDiscontinued")
+                        };
+                    };
+                };
+            };
+            return null;
         }
 
         protected override void RemoveCore( int id )
         {
-            throw new NotImplementedException();
+            using (var conn = OpenDatabase())
+            {
+                //Alternative approach to creating command
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "RemoveProduct";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //Long way
+                var parameter = new SqlParameter("@id", id);
+                cmd.Parameters.Add(parameter);
+
+                cmd.ExecuteNonQuery();
+            };
         }
 
-        protected override Product UpdateCore( Product existing, Product newItem )
+        protected override Product UpdateCore( Product existing, Product product )
         {
-            throw new NotImplementedException();
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("UpdateProduct", conn) { CommandType = CommandType.StoredProcedure };
+
+                cmd.Parameters.AddWithValue("@id", existing.Id);
+                cmd.Parameters.AddWithValue("@name",product.Name);
+                cmd.Parameters.AddWithValue("@Description", product.Description);
+                cmd.Parameters.AddWithValue("@price", product.Price);
+                cmd.Parameters.AddWithValue("@isDiscontinued", product.IsDiscontinued);
+
+                cmd.ExecuteNonQuery();
+
+            };
+            return GetCore(existing.Id);
         }
 
         private SqlConnection OpenDatabase()
