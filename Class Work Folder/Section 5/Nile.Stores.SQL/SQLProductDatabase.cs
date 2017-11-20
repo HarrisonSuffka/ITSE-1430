015e4d8d4b93ae66c0792e453d418abc647b335a
Harrison.Suffka@my.tccd.edu <Harrison.Suffka@my.tccd.edu>
2017-11-20 17:04:15 -0600

    11/20/2017

--- Class Work Folder/Section 5/Nile.Stores.SQL/SQLProductDatabase.cs
+++ Class Work Folder/Section 5/Nile.Stores.SQL/SQLProductDatabase.cs
@@ -6,14 +6,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Nile.Stores.Sql
+namespace Nile.Stores.SQL
 {
-    /// <summary>Provides an implementation of <see cref="IProductDatabase"/> using SQL Server.</summary>
-    public class SqlProductDatabase : ProductDatabase
+    /// <summary> Provides an implantation of <see cref="IPrdoductDatabase"/> using SQL Server </summary>
+    public class SQLProductDatabase : ProductDatabase
     {
+
         #region Construction
 
-        public SqlProductDatabase ( string connectionString )
+        public SQLProductDatabase ( string connectionString)
         {
             _connectionString = connectionString;
         }
@@ -26,17 +27,16 @@ namespace Nile.Stores.Sql
             var id = 0;
             using (var conn = OpenDatabase())
             {
-                var cmd = new SqlCommand("AddProduct", conn) 
-                { CommandType = CommandType.StoredProcedure };
+                var cmd = new SqlCommand("AddProduct", conn) { CommandType = CommandType.StoredProcedure };
 
                 cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = product.Name;
-                cmd.Parameters.AddWithValue("@description", product.Description);
+                cmd.Parameters.AddWithValue("@Description", product.Description);
                 cmd.Parameters.AddWithValue("@price", product.Price);
                 cmd.Parameters.AddWithValue("@isDiscontinued", product.IsDiscontinued);
 
                 id = Convert.ToInt32(cmd.ExecuteScalar());
-            };
 
+            };
             return GetCore(id);
         }
 
@@ -56,12 +56,11 @@ namespace Nile.Stores.Sql
                         //reader.GetName(0);
                         //reader.GetFieldType(1);
                         //Convert.ToInt32(reader["Id"]);
-                        var product = new Product() 
-                        {
+                        var product = new Product() {
                             Id = reader.IsDBNull(0) ? 0 : reader.GetInt32(0),
                             Name = reader.GetFieldValue<string>(1),
                             Price = reader.GetDecimal(2),
-                            Description = reader.GetString(3),                            
+                            Description = reader.GetString(3),
                             IsDiscontinued = reader.GetBoolean(4)
                         };
                         products.Add(product);
@@ -70,6 +69,7 @@ namespace Nile.Stores.Sql
 
                 return products;
             };
+
         }
 
         protected override Product GetCore( int id )
@@ -77,16 +77,13 @@ namespace Nile.Stores.Sql
             using (var conn = OpenDatabase())
             {
                 var cmd = new SqlCommand("GetProduct", conn) { CommandType = CommandType.StoredProcedure };
+
                 cmd.Parameters.AddWithValue("@id", id);
 
-                //Using a dataset instead of a reader
                 var ds = new DataSet();
-                var da = new SqlDataAdapter() {
-                    SelectCommand = cmd,
-                    //DeleteCommand,
-                    //UpdateCommand,
-                    //InsertCommand,
-
+                var da = new SqlDataAdapter()
+                {
+                    SelectCommand = cmd
                 };
 
                 da.Fill(ds);
@@ -101,27 +98,21 @@ namespace Nile.Stores.Sql
                             Id = Convert.ToInt32(row["Id"]),
                             Name = row.Field<string>("Name"),
                             Description = row.Field<string>("Description"),
-                            Price = row.Field<decimal>("price"),
-                            IsDiscontinued = row.Field<bool>("isdiscontinued")
+                            Price = row.Field<decimal>("Price"),
+                            IsDiscontinued = row.Field<bool>("isDiscontinued")
                         };
                     };
-                };  
+                };
             };
-
             return null;
         }
 
         protected override void RemoveCore( int id )
         {
             using (var conn = OpenDatabase())
-            {                
-                //var input = "0'; DELETE FROM Products;SELECT '";
-                //var commandText = $"DELETE FROM Products WHERE Id = '{input}'";
-                // DELETE FROM Products WHERE Id = '0'; DELETE FROM Products;SELECT ''
-
+            {
                 //Alternative approach to creating command
                 var cmd = conn.CreateCommand();
-                //cmd.CommandText = "DELETE FROM Products WHERE Id = @id";
                 cmd.CommandText = "RemoveProduct";
                 cmd.CommandType = CommandType.StoredProcedure;
 
@@ -140,19 +131,21 @@ namespace Nile.Stores.Sql
                 var cmd = new SqlCommand("UpdateProduct", conn) { CommandType = CommandType.StoredProcedure };
 
                 cmd.Parameters.AddWithValue("@id", existing.Id);
-                cmd.Parameters.AddWithValue("@name", product.Name);                
-                cmd.Parameters.AddWithValue("@description", product.Description);
+                cmd.Parameters.AddWithValue("@name",product.Name);
+                cmd.Parameters.AddWithValue("@Description", product.Description);
                 cmd.Parameters.AddWithValue("@price", product.Price);
                 cmd.Parameters.AddWithValue("@isDiscontinued", product.IsDiscontinued);
 
                 cmd.ExecuteNonQuery();
-            };
 
-            return GetCore(existing.Id);            
+            };
+            return GetCore(existing.Id);
         }
 
-        private SqlConnection OpenDatabase ()
+        private SqlConnection OpenDatabase()
         {
+            //var conString = @"Server=(localdb)\ProjectsV13;Database=NileDatabase;Integrated Security=True;";
+
             var connection = new SqlConnection(_connectionString);
 
             connection.Open();
---
+++ Class Work Folder/Section 5/Nile/ProductDatabaseExtensions.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * ITSE 1430
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nile
+{
+    /// <summary>Provides extensions for <see cref="IProductDatabase"/>.</summary>
+    public static class ProductDatabaseExtensions
+    {
+        /// <summary>Get a product by name.</summary>
+        /// <param name="source">The source.</param>
+        /// <param name="name">The product name.</param>
+        /// <returns>The product, if found.</returns>
+        public static Product GetByName ( this IProductDatabase source, string name )
+        {
+            foreach (var item in source.GetAll())
+            {
+                if (String.Compare(item.Name, name, true) == 0)
+                    return item;
+            };
+
+            return null;
+        }
+
+        public static IEnumerable<Product> GetProductDiscountPrice( this IProductDatabase source, Func<Product, Decimal> priceCalculator )
+        {
+            var products = from product in source.GetAll()
+                           where product.IsDiscontinued
+                           select new {
+                               Product = product,
+                               AdjustedPrice = product.IsDiscontinued ? priceCalculator(product) : product.Price
+                           };
+
+            return from product in products
+                   orderby product.AdjustedPrice
+                   select product.Product;
+        }
+
+
+
+        //private sealed class SomeType
+        //{
+        //    public Product Product { get; set; }
+        //    public decimal AdjustedPrice { get; set; }
+        //}
+
+        /// <summary>Adds seed data to a database.</summary>
+        /// <param name="source">The data to seed.</param>
+        public static void WithSeedData ( this IProductDatabase source )
+        {
+            source.Add(new Product() { Name = "Galaxy S7", Price = 650 });
+            source.Add(new Product() { Name = "Galaxy Note 7", Price = 150, IsDiscontinued = true });
+            source.Add(new Product() { Name = "Windows Phone", Price = 100 });
+            source.Add(new Product() { Name = "iPhone X", Price = 1900, IsDiscontinued = true });
+        }
+    }
+}
