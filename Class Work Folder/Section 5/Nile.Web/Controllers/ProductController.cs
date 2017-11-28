3e005c6d233845054b8b4ae5c2fb9f5cbc54f11b
Harrison.Suffka@my.tccd.edu <Harrison.Suffka@my.tccd.edu>
2017-11-27 19:03:03 -0600

    11/27/2017

--- Class Work Folder/Section 5/Nile.Web/Controllers/ProductController.cs
+++ Class Work Folder/Section 5/Nile.Web/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Nile.Web.Models;
 
 namespace Nile.Web.Controllers
 {
@@ -11,7 +12,8 @@ namespace Nile.Web.Controllers
         // GET: Product
         public ActionResult Index()
         {
-            return View();
+            var model = new List<ProductViewModel>() { new ProductViewModel() { Id = 1, Name = "Product A", Price = 123 } };
+            return View(model);
         }
     }
 }
