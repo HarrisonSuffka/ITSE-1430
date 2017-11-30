d433bc105268be0bcd7129f69f01f703abf3c431
Harrison.Suffka@my.tccd.edu <Harrison.Suffka@my.tccd.edu>
2017-11-29 18:35:46 -0600

    11/29/2017

---
+++ Professor/ClassWork/Section5/Nile.Web/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nile.Web.Models;
+
+namespace Nile.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ProductController (IProductDatabase database)
+        {
+            _database = database;
+        }
+
+        // GET: Product
+        public ActionResult List()
+        {
+            var products = _database.GetAll();
+            return View(products.ToModel());
+        }
+
+        private readonly IProductDatabase _database;
+    }
+}
